a1565024d4a1e7b11b3e87adaeaa3707bccd160f
RBO <rbo@MacBook-Pro-3.local>
2024-07-16 16:54:41 +0200

    add img and color attributes to scripts

--- setup.js
+++ setup.js
@@ -1,23 +1,33 @@
 (function () {
-    var currentScript = document.currentScript || (function () {
-        return document.getElementsByTagName('script')[document.getElementsByTagName('script').length - 1];
-    })();
+  var currentScript =
+    document.currentScript ||
+    (function () {
+      return document.getElementsByTagName("script")[
+        document.getElementsByTagName("script").length - 1
+      ]
+    })()
 
-    var shopifyStoreName = currentScript.getAttribute('data-shopify-id');
-    if (!shopifyStoreName) {
-        console.error('data-shopify-id was not provided.');
-        return;
-    }
+  var shopifyStoreName = currentScript.getAttribute("data-shopify-id")
+  var shopifyAvatarImage =
+    currentScript.getAttribute("data-avatar-image") ??
+    "https://i.imgur.com/dfUvzRb.png"
+  var shopifyColor = currentScript.getAttribute("data-color") ?? "#A1BB58"
+  if (!shopifyStoreName) {
+    console.error("data-shopify-id was not provided.")
+    return
+  }
 
-    window.RBO = window.RBO || {};
-    window.RBO.shopifyStore = shopifyStoreName;
+  window.RBO = window.RBO || {}
+  window.RBO.shopifyStore = shopifyStoreName
+  window.RBO.shopifyAvatarImage = shopifyAvatarImage
+  window.RBO.shopifyColor = shopifyColor
 
-    var scriptURL = "https://cdn.jsdelivr.net/gh/ready-bot-one/widget/rbo.js";
-    var scriptElement = document.createElement('script');
-    scriptElement.src = scriptURL;
-    scriptElement.onerror = function () {
-        console.error('Failed to load the script: ' + scriptURL);
-    };
+  var scriptURL = "https://cdn.jsdelivr.net/gh/ready-bot-one/widget/rbo.js"
+  var scriptElement = document.createElement("script")
+  scriptElement.src = scriptURL
+  scriptElement.onerror = function () {
+    console.error("Failed to load the script: " + scriptURL)
+  }
 
-    document.head.appendChild(scriptElement);
+  document.head.appendChild(scriptElement)
 })();

6ae833bf0af36cfe176f22e2c704734edf6a21d4
RBO <rbo@MacBook-Pro-4.local>
2025-02-01 18:36:43 +0100

    adjust avatar obj for vf snippet

--- rbo.js
+++ rbo.js
@@ -2,15 +2,19 @@
       var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
       v.onload = function() {
         window.voiceflow.chat.load({
-          verify: { projectID: 'XXX' },
-          url: 'https://general-runtime.voiceflow.com',
-          versionID: 'production',
+          verify: { projectID: "XXX" },
+          url: "https://general-runtime.voiceflow.com",
+          versionID: "production",
           assistant: {
-            persistence: 'memory',
+            persistence: "memory",
             color: `${window.RBO.shopifyColor}`,
             image: `${window.RBO.shopifyAvatarImage}`,
-            avatar: `${window.RBO.shopifyAvatarImage}`,
-            stylesheet: "https://cdn.jsdelivr.net/gh/ready-bot-one/widget/rbo.css",
+            avatar: {
+              hide: false,
+              imageUrl: `${window.RBO.shopifyAvatarImage}`,
+            },
+            stylesheet:
+              "https://cdn.jsdelivr.net/gh/ready-bot-one/widget/rbo.css",
           },
           launch: {
             event: {
@@ -20,7 +24,7 @@
               },
             },
           },
-        });
+        })
       }
       v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; 
       v.type = "text/javascript"; s.parentNode.insertBefore(v, s);

0ffb75827df4da7d26cca39778a8ab67d2b679f9
RBO <rbo@MacBook-Pro-4.local>
2025-02-01 18:45:02 +0100

    revert to old widget

--- rbo.js
+++ rbo.js
@@ -1,36 +1,28 @@
-;;(function(d, t) {
-      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
-      v.onload = function() {
-        window.voiceflow.chat.load({
-          verify: { projectID: "65d930a9e996d1330850cd32" },
-          url: "https://proxy.readybotone.io",
-          versionID: "production",
-          assistant: {
-            persistence: "memory",
-            color: `${window.RBO.shopifyColor}`,
-            image: `${window.RBO.shopifyAvatarImage}`,
-            avatar: {
-              hide: false,
-              imageUrl: `${window.RBO.shopifyAvatarImage}`,
-            },
-            stylesheet:
-              "https://cdn.jsdelivr.net/gh/ready-bot-one/widget/rbo.css",
+;(function (d, t) {
+  var v = d.createElement(t),
+    s = d.getElementsByTagName(t)[0]
+  v.onload = function () {
+    window.voiceflow.chat.load({
+      verify: { projectID: "65d930a9e996d1330850cd32" },
+      url: "https://proxy.readybotone.io",
+      versionID: "production",
+      assistant: {
+        color: `${window.RBO.shopifyColor}`,
+        image: `${window.RBO.shopifyAvatarImage}`,
+        avatar: `${window.RBO.shopifyAvatarImage}`,
+        stylesheet: "https://cdn.jsdelivr.net/gh/ready-bot-one/widget/rbo.css",
+      },
+      launch: {
+        event: {
+          type: "launch",
+          payload: {
+            shopify_store: window.RBO.shopifyStore,
           },
-          launch: {
-            event: {
-              type: "launch",
-              payload: {
-                shopify_store: window.RBO.shopifyStore,
-              },
-            },
-          },
-        })
-      }
-      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; 
-      v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
-    })(document, 'script');
-
-
-
-
-  
+        },
+      },
+    })
+  }
+  v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"
+  v.type = "text/javascript"
+  s.parentNode.insertBefore(v, s)
+})(document, "script")

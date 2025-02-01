b07a6f22ad5575e393a383dacbbfd5f1b076feeb
devbotone <160746240+devbotone@users.noreply.github.com>
2025-02-01 18:19:14 +0100

    Update rbo.js for new widget

--- rbo.js
+++ rbo.js
@@ -1,30 +1,32 @@
-;;(function (d, t) {
-  var v = d.createElement(t),
-    s = d.getElementsByTagName(t)[0]
-  v.onload = function () {
-    window.voiceflow.chat.load({
-      verify: { projectID: "65d930a9e996d1330850cd32" },
-      url: "https://proxy.readybotone.io",
-      versionID: "production",
-      assistant: {
-        color: `${window.RBO.shopifyColor}`,
-        image: `${window.RBO.shopifyAvatarImage}`,
-        avatar: `${window.RBO.shopifyAvatarImage}`,
-        stylesheet: "https://cdn.jsdelivr.net/gh/ready-bot-one/widget/rbo.css",
-      },
-      launch: {
-        event: {
-          type: "launch",
-          payload: {
-            shopify_store: window.RBO.shopifyStore,
+;;(function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: 'XXX' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          assistant: {
+            persistence: 'memory',
+            color: `${window.RBO.shopifyColor}`,
+            image: `${window.RBO.shopifyAvatarImage}`,
+            avatar: `${window.RBO.shopifyAvatarImage}`,
+            stylesheet: "https://cdn.jsdelivr.net/gh/ready-bot-one/widget/rbo.css",
           },
-        },
-      },
-    })
-  }
-  v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"
-  v.type = "text/javascript"
-  s.parentNode.insertBefore(v, s)
-})(document, "script")
+          launch: {
+            event: {
+              type: "launch",
+              payload: {
+                shopify_store: window.RBO.shopifyStore,
+              },
+            },
+          },
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; 
+      v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+    })(document, 'script');
 
 
+
+
+  
